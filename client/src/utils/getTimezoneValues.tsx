ddfc34ed7e68e18c12d048b9ceb4ac7465ad0ec6
dimanrikalov <93448948+dimanrikalov@users.noreply.github.com>
2023-07-12 11:36:17 +0300

    Removed unnecessary filtering

--- client/src/utils/getTimezoneValues.tsx
+++ client/src/utils/getTimezoneValues.tsx
@@ -2,13 +2,11 @@ interface SortedEntry {
 	[key: string]: string[];
 }
 
-export const extractNeutralValues = (
-	arr: SortedEntry[]
-): SortedEntry => {
+export const extractNeutralValues = (arr: SortedEntry[]): SortedEntry => {
 	const UTC = arr.find((x) => Object.keys(x)[0] === 'UTC');
 	const UTC0 = arr.find((x) => Object.keys(x)[0] === 'UTC+00:00');
 
-	if(!UTC || !UTC0) {
+	if (!UTC || !UTC0) {
 		return {};
 	}
 
@@ -20,10 +18,15 @@ export const extractNeutralValues = (
 	};
 };
 
-export const extractPositiveValues = (arr: { [key: string]: string[] }[]) : SortedEntry[] =>
+export const extractPositiveValues = (
+	arr: { [key: string]: string[] }[]
+): SortedEntry[] =>
 	arr
-		.filter((x) => Object.keys(x)[0].includes('+'))
-		.filter((x) => Object.keys(x)[0] !== 'UTC+00:00')
+		.filter(
+			(x) =>
+				Object.keys(x)[0].includes('+') &&
+				Object.keys(x)[0] !== 'UTC+00:00'
+		)
 		.sort((a, b) => {
 			const utcA = Object.keys(a)[0];
 			const utcB = Object.keys(b)[0];
@@ -36,7 +39,9 @@ export const extractPositiveValues = (arr: { [key: string]: string[] }[]) : Sort
 			return 0;
 		});
 
-export const extractNegativeValues = (arr: { [key: string]: string[] }[])  : SortedEntry[] =>
+export const extractNegativeValues = (
+	arr: { [key: string]: string[] }[]
+): SortedEntry[] =>
 	arr
 		.filter((x) => Object.keys(x)[0].includes('-'))
 		.sort((a, b) => {
